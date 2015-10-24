2c13990f0c4200e9c17c581fea092def7d28f3cf
qoo392 <qoo392@hotmail.com>
2015-10-24 12:30:42 +0000

    make return content neatly

--- lib/second_handler.rb
+++ lib/second_handler.rb
@@ -2,15 +2,50 @@ require 'koala'
 require 'json'
 
 module SecondHandler
-  class FbGroupPost
+  class FbGroupPost 
     def initialize (access_token, group_id)
       @graph = Koala::Facebook::API.new(access_token)
       @group_id = group_id
     end
+   
+    def first_page
+      @feed = @graph.get_connections(@group_id, "feed", :fields => ["attachments{media,subattachments{media}}","id","message","updated_time"])
+    end
+        
+    def next_page
+      @feed = @feed.next_page
+    end
+    
+    def previous_page
+      @feed = @feed.previous_page
+    end
+    
+    # return feed of current page  infomation , including image
+    def get_content
+      @feed.map do |item|
+        tmp = Hash.new
+        tmp["id"] = item["id"]
+        tmp["message"] = item["message"]
+        tmp["updated_time"] = item["updated_time"]
+        tmp["attachments"] = attachment_helper(item["attachments"])
+        tmp
+      end
+   
+    end
 
-   # return feed collecton , can get next page by content.next_page
-   def content
-      feed = @graph.get_connections(@group_id, "feed", :fields => ["message","attachments","updated_time","comments"])
+    private
+    
+    def attachment_helper (attach_hash)
+      ok_data = []
+      if attach_hash && attach_hash["data"].first["media"]
+        ok_data << attach_hash["data"].first["media"]
+      end
+      if attach_hash && attach_hash["data"].first["subattachments"]
+        attach_hash["data"].first["subattachments"].each do |item|
+          ok_data << item
+        end
+      end
+      ok_data
     end
   end
 end
